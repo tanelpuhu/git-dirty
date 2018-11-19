b9fd191f9b294180c9b70c2b296cb8db7718c03d
Tanel Puhu <tanel@tanelpuhu.com>
2018-11-19 21:03:25 +0000

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+## git-dirty
+
+`go get github.com/tanelpuhu/git-dirty`
+
+### Example
+
+```bash
+$ which git-dirty
+/Users/tanel/src/go/bin/git-dirty
+$ git dirty ${GOPATH}/src/github.com/tanelpuhu
+/Users/tanel/src/go/src/github.com/tanelpuhu/git-dirty
+  A  README.md
+  AM main.go
+```
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+const gitExecutable = "git"
+
+var count int
+
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func chdir(path string) {
+	if err := os.Chdir(path); err != nil {
+		panic(err)
+	}
+}
+
+func gitDirty(path string) {
+	wd, _ := os.Getwd()
+	defer chdir(wd)
+	chdir(path)
+	output, err := exec.Command(gitExecutable, "status", "--short").CombinedOutput()
+	if err != nil {
+		fmt.Printf("%s\n", output)
+		logrus.Fatal(err)
+	}
+	if len(output) > 0 {
+		fmt.Println(path)
+		for _, line := range strings.Split(strings.TrimSpace(string(output)), "\n") {
+			fmt.Printf("  %s\n", line)
+		}
+		count++
+	}
+}
+
+func walk(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		logrus.Fatalf("error walking %s: %v", path, err)
+	}
+	if info.IsDir() && info.Name() == ".git" {
+		basepath, err := filepath.Abs(filepath.Dir(path))
+		if err != nil {
+			logrus.Fatalf("error getting absolute directory for %s: %v", path, err)
+		}
+		if fileExists(filepath.Join(path, "HEAD")) && fileExists(filepath.Join(path, "refs")) {
+			gitDirty(basepath)
+		}
+		return filepath.SkipDir
+	}
+	return nil
+}
+
+func init() {
+	_, err := exec.LookPath(gitExecutable)
+	if err != nil {
+		logrus.Fatalf("%s not present in $PATH", gitExecutable)
+	}
+	flag.Parse()
+}
+
+func main() {
+	args := flag.Args()
+	if len(flag.Args()) == 0 {
+		args = append(args, ".")
+	}
+	for _, arg := range args {
+		filepath.Walk(arg, walk)
+	}
+	if count > 0 {
+		os.Exit(1)
+	}
+}
